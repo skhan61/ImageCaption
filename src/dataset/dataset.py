34cabff984504733562e63843faaafe4e529c88e
skhan61 <sayem.eee.kuet@outlook.com>
2023-12-07 15:06:17 -0500

    Automatic commit at Thu 07 Dec 2023 03:06:17 PM EST

---
+++ src/dataset/dataset.py
@@ -0,0 +1,89 @@
+import os
+
+import nltk
+import torch
+from nltk.tokenize import word_tokenize
+from PIL import Image
+from pycocotools.coco import COCO
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+# nltk.download('punkt')
+
+
+class Vocabulary:
+    """Simple vocabulary wrapper."""
+
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+        self.add_word('<pad>')  # Padding
+        self.add_word('<start>')  # Start token
+        self.add_word('<end>')  # End token
+        self.add_word('<unk>')  # Unknown token
+
+    def add_word(self, word):
+        if not word in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def __call__(self, word):
+        return self.word2idx.get(word, self.word2idx['<unk>'])
+
+    def __len__(self):
+        return len(self.word2idx)
+
+
+class CocoCaptionsDataset(Dataset):
+    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+
+    def __init__(self, root_dir, ann_file, vocab, transform=None):
+        self.root_dir = root_dir
+        self.coco = COCO(ann_file)
+        self.ids = list(self.coco.anns.keys())
+        self.vocab = vocab
+        self.transform = transform
+
+    def __getitem__(self, index):
+        ann_id = self.ids[index]
+        caption = self.coco.anns[ann_id]['caption']
+        img_id = self.coco.anns[ann_id]['image_id']
+        path = self.coco.loadImgs(img_id)[0]['file_name']
+
+        image = Image.open(os.path.join(self.root_dir, path)).convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+
+        # Convert caption to word ids.
+        tokens = word_tokenize(str(caption).lower())
+        caption = []
+        caption.append(self.vocab('<start>'))
+        caption.extend([self.vocab(token) for token in tokens])
+        caption.append(self.vocab('<end>'))
+        target = torch.Tensor(caption)
+
+        return image, target
+
+    def __len__(self):
+        return len(self.ids)
+
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (image, caption)."""
+    # Sort a data list by caption length (descending order)
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions = zip(*data)
+
+    # Merge images (convert tuple of 3D tensor to 4D tensor)
+    images = torch.stack(images, 0)
+
+    # Merge captions (convert tuple of 1D tensor to 2D tensor)
+    # Also record caption lengths (before padding)
+    lengths = [len(cap) for cap in captions]
+    captions = [torch.Tensor(cap) for cap in captions]
+    captions = pad_sequence(captions, batch_first=True)
+
+    return images, captions, torch.tensor(lengths)
---
+++ src/models/enocders.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models import ResNet152_Weights
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        # Load pre-trained ResNet-152 using the new API
+        weights = ResNet152_Weights.IMAGENET1K_V1  # or use ResNet152_Weights.DEFAULT
+        resnet = models.resnet152(weights=weights)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+
+    def forward(self, images):
+        with torch.no_grad():
+            features = self.resnet(images)
+        features = features.reshape(features.size(0), -1)
+        features = self.linear(features)
+        return self.bn(features)
+        return self.bn(features)
+        return self.bn(features)
