79aadd231b0e5df8f3ad7a7fb5d324d9747138e7
skhan61 <sayem.eee.kuet@outlook.com>
2023-12-11 22:47:30 -0500

    Automatic commit at Mon 11 Dec 2023 10:47:30 PM EST

--- src/models/encoders.py
+++ src/models/encoders.py
@@ -1,3 +1,4 @@
+from torchvision import models
 import torch
 import torch.nn as nn
 import torchvision.models as models
@@ -13,6 +14,11 @@ class EncoderCNN(nn.Module):
         weights = ResNet152_Weights.IMAGENET1K_V1  # or use ResNet152_Weights.DEFAULT
         resnet = models.resnet152(weights=weights)
         modules = list(resnet.children())[:-1]
+
+        # Freeze the ResNet layers to prevent backpropagation (if not fine-tuning)
+        for param in resnet.parameters():
+            param.requires_grad = False
+
         self.resnet = nn.Sequential(*modules)
         self.linear = nn.Linear(resnet.fc.in_features, embed_size)
         self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
@@ -26,38 +32,129 @@ class EncoderCNN(nn.Module):
 
 
 class ModifiedEncoderCNN(nn.Module):
-    def __init__(self, embed_size, group_elements):
+    def __init__(self, embed_size, group_conv_layer):
         super(ModifiedEncoderCNN, self).__init__()
+        self.group_conv = group_conv_layer  # Use the provided GroupConvLayer instance
 
-        # Define GroupConvLayer with appropriate channel sizes and group elements
-        self.group_conv = GroupConvLayer(
-            in_channels=3, out_channels=64, kernel_size=7, group=group_elements)
-
-        # Load pre-trained ResNet-152, excluding the first conv layer and fully connected layers
+        # Load pre-trained ResNet-152
         weights = ResNet152_Weights.IMAGENET1K_V1
         resnet = models.resnet152(weights=weights)
-        # Exclude the first conv layer and fully connected layers
+        # Exclude the first /conv layer and the last fully connected layer
         modules = list(resnet.children())[1:-1]
+
+        # Freeze the ResNet layers to prevent backpropagation (if not fine-tuning)
+        for param in resnet.parameters():
+            param.requires_grad = False
+
         self.resnet = nn.Sequential(*modules)
 
-        # Redefine the linear layer to match the output of the modified ResNet architecture
+        # Adjust the number of output channels to match ResNet's input channels
+        self.adjust_channels = nn.Conv2d(1, 64, kernel_size=1)
+
         self.linear = nn.Linear(resnet.fc.in_features, embed_size)
         self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
 
     def forward(self, images):
-        # First, pass images through the GroupConvLayer
         group_features = self.group_conv(images)
+        features = torch.mean(group_features, dim=1, keepdim=True)
 
-        # Process the output of GroupConvLayer as needed (e.g., reshape, aggregate)
-        # For simplicity, let's assume we aggregate the group features by averaging
-        features = torch.mean(group_features, dim=1)
+        # Adjust the channel size to match ResNet's expected input
+        features = self.adjust_channels(features)
 
-        # Continue with the modified ResNet layers
         with torch.no_grad():
             features = self.resnet(features)
+
         features = features.reshape(features.size(0), -1)
         features = self.linear(features)
         return self.bn(features)
+
+
+# class ModifiedEncoderCNN(nn.Module):
+#     def __init__(self, embed_size, group_elements):
+#         super(ModifiedEncoderCNN, self).__init__()
+#         self.group_conv = GroupConvLayer(
+#             in_channels=3, out_channels=64,
+#             kernel_size=7, group=group_elements,
+#             learnable_rotations=True)
+
+#         # Load pre-trained ResNet-152
+#         weights = ResNet152_Weights.IMAGENET1K_V1
+#         resnet = models.resnet152(weights=weights)
+#         # Exclude the first /conv layer and the last fully connected layer
+#         modules = list(resnet.children())[1:-1]
+
+#         # Freeze the ResNet layers to prevent backpropagation (if not fine-tuning)
+#         for param in resnet.parameters():
+#             param.requires_grad = False
+
+#         self.resnet = nn.Sequential(*modules)
+
+#         # Adjust the number of output channels to match ResNet's input channels
+#         self.adjust_channels = nn.Conv2d(1, 64, kernel_size=1)
+
+#         self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+#         self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+
+#     def forward(self, images):
+#         group_features = self.group_conv(images)
+#         features = torch.mean(group_features, dim=1, keepdim=True)
+
+#         # Adjust the channel size to match ResNet's expected input
+#         features = self.adjust_channels(features)
+
+#         with torch.no_grad():
+#             features = self.resnet(features)
+
+#         features = features.reshape(features.size(0), -1)
+#         features = self.linear(features)
+#         return self.bn(features)
+
+    # def forward(self, images):
+    #     group_features = self.group_conv(images)
+    #     features = torch.mean(group_features, dim=1)  # Aggregate features
+
+    #     with torch.no_grad():  # Prevent gradient tracking
+    #         features = self.resnet(features)
+
+    #     features = features.reshape(features.size(0), -1)
+    #     features = self.linear(features)
+    #     return self.bn(features)
+
+
+# class ModifiedEncoderCNN(nn.Module):
+#     def __init__(self, embed_size, group_elements):
+#         super(ModifiedEncoderCNN, self).__init__()
+
+#         # Define GroupConvLayer with appropriate channel sizes and group elements
+#         self.group_conv = GroupConvLayer(
+#             in_channels=3, out_channels=64, kernel_size=7, group=group_elements)
+
+#         # Load pre-trained ResNet-152, excluding the first conv layer and fully connected layers
+#         weights = ResNet152_Weights.IMAGENET1K_V1
+#         resnet = models.resnet152(weights=weights)
+
+#         # Exclude the first conv layer and fully connected layers
+#         modules = list(resnet.children())[1:-1]
+#         self.resnet = nn.Sequential(*modules)
+
+#         # Redefine the linear layer to match the output of the modified ResNet architecture
+#         self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+#         self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+
+#     def forward(self, images):
+#         # First, pass images through the GroupConvLayer
+#         group_features = self.group_conv(images)
+
+#         # Process the output of GroupConvLayer as needed (e.g., reshape, aggregate)
+#         # For simplicity, let's assume we aggregate the group features by averaging
+#         features = torch.mean(group_features, dim=1)
+
+#         # Continue with the modified ResNet layers
+#         with torch.no_grad():
+#             features = self.resnet(features)
+#         features = features.reshape(features.size(0), -1)
+#         features = self.linear(features)
+#         return self.bn(features)
 
 
 # # Define group elements (e.g., rotation angles)
--- src/models/group_cnn_layer.py
+++ src/models/group_cnn_layer.py
@@ -6,44 +6,173 @@ import torch.nn.functional as F
 
 
 class GroupConvLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, group):
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 group, learnable_rotations=False):
         super(GroupConvLayer, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels,
-                              kernel_size, padding=kernel_size//2)
-        self.group = group  # e.g., [0, 90, 180, 270] for rotations
+                              kernel_size, padding=kernel_size // 2)
+        self.group = group
+        self.learnable_rotations = learnable_rotations
+        self.rotation_matrices = self._precompute_rotation_matrices(group)
 
-    def transform_input(self, x, group_element):
-        # Convert degrees to radians
-        radians = group_element * math.pi / 180
+        if self.learnable_rotations:
+            # Replace fixed rotation angles with learnable parameters
+            self.rotation_angles = nn.Parameter(torch.tensor(
+                [g * math.pi / 180 for g in group], dtype=torch.float32))
 
-        # Convert radians to a tensor
-        radians = torch.tensor(radians, dtype=torch.float32)
+    def _precompute_rotation_matrices(self, group):
+        matrices = []
+        for group_element in group:
+            radians = group_element * math.pi / 180
+            rotation_matrix = self._create_rotation_matrix(radians)
+            matrices.append(rotation_matrix)
+        # Stack and convert to a parameter
+        return nn.Parameter(torch.stack(matrices), requires_grad=False)
 
-        # Create rotation matrix
-        rotation_matrix = torch.tensor([
-            [torch.cos(radians), -torch.sin(radians), 0],
-            [torch.sin(radians), torch.cos(radians), 0]
+    @staticmethod
+    def _create_rotation_matrix(radians):
+        # Check if 'radians' is already a tensor. If not, convert it to a tensor.
+        if not isinstance(radians, torch.Tensor):
+            radians_tensor = torch.tensor(radians, dtype=torch.float32)
+        else:
+            # If it's already a tensor, use it directly.
+            radians_tensor = radians
+
+        return torch.tensor([
+            [torch.cos(radians_tensor), -torch.sin(radians_tensor), 0],
+            [torch.sin(radians_tensor), torch.cos(radians_tensor), 0]
         ], dtype=torch.float32)
 
-        # Repeat the rotation matrix for each image in the batch
+    def transform_input(self, x, rotation_matrix):
         batch_size = x.size(0)
-        rotation_matrix = rotation_matrix.repeat(batch_size, 1, 1)
-
-        # Create the grid for rotation
+        # Repeat rotation matrix for each batch element and reshape
+        rotation_matrix = rotation_matrix.repeat(
+            batch_size, 1, 1).view(batch_size, 2, 3)
         grid = F.affine_grid(rotation_matrix, x.size(), align_corners=True)
         return F.grid_sample(x, grid, align_corners=True)
 
     def forward(self, x):
         outputs = []
-        for group_element in self.group:
-            transformed_x = self.transform_input(x, group_element)
+
+        if self.learnable_rotations:
+            # Update rotation matrices if they are learnable
+            updated_matrices = [self._create_rotation_matrix(
+                angle).to(x.device) for angle in self.rotation_angles]
+        else:
+            updated_matrices = self.rotation_matrices
+
+        for rotation_matrix in updated_matrices:
+            transformed_x = self.transform_input(x, rotation_matrix)
             conv_output = self.conv(transformed_x)
             outputs.append(conv_output)
-        out = torch.stack(outputs, dim=1)  # Stack along a new dimension
+
+        # Concatenate along a new dimension for memory efficiency
+        out = torch.cat(outputs, dim=1)
         return out
 
 
-# from models.group_cnn_layer import GroupConvLayer
+class SqueezeExcitation(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SqueezeExcitation, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class GroupConvLayerWithAttention(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 group, learnable_rotations=False):
+        super(GroupConvLayerWithAttention, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels,
+                              kernel_size, padding=kernel_size // 2)
+        self.group = group
+        self.learnable_rotations = learnable_rotations
+        self.rotation_matrices = self._precompute_rotation_matrices(group)
+        self.attention_blocks = nn.ModuleList(
+            [SqueezeExcitation(out_channels) for _ in group])
+
+        if self.learnable_rotations:
+            self.rotation_angles = nn.Parameter(torch.tensor(
+                [g * math.pi / 180 for g in group], dtype=torch.float32))
+
+    def _precompute_rotation_matrices(self, group):
+        matrices = []
+        for group_element in group:
+            radians = group_element * math.pi / 180
+            rotation_matrix = self._create_rotation_matrix(radians)
+            matrices.append(rotation_matrix)
+        return nn.Parameter(torch.stack(matrices), requires_grad=False)
+
+    @staticmethod
+    def _create_rotation_matrix(radians):
+        if not isinstance(radians, torch.Tensor):
+            radians_tensor = torch.tensor(radians, dtype=torch.float32)
+        else:
+            radians_tensor = radians
+        return torch.tensor([[torch.cos(radians_tensor), -torch.sin(radians_tensor), 0],
+                             [torch.sin(radians_tensor), torch.cos(radians_tensor), 0]],
+                            dtype=torch.float32)
+
+    def transform_input(self, x, rotation_matrix):
+        batch_size = x.size(0)
+        rotation_matrix = rotation_matrix.repeat(
+            batch_size, 1, 1).view(batch_size, 2, 3)
+        grid = F.affine_grid(rotation_matrix, x.size(), align_corners=True)
+        return F.grid_sample(x, grid, align_corners=True)
+
+    def forward(self, x):
+        outputs = []
+        updated_matrices = self.rotation_matrices if not self.learnable_rotations else \
+            [self._create_rotation_matrix(angle).to(
+                x.device) for angle in self.rotation_angles]
+
+        for i, rotation_matrix in enumerate(updated_matrices):
+            transformed_x = self.transform_input(x, rotation_matrix)
+            conv_output = self.conv(transformed_x)
+            attention_output = self.attention_blocks[i](conv_output)
+            outputs.append(attention_output)
+
+        out = torch.cat(outputs, dim=1)
+        return out
+
+
+# import torch
+# from models.group_cnn_layer import GroupConvLayerWithAttention
+
+# # Create a dummy input
+# dummy_input = torch.randn(1, 3, 64, 64)  # Batch size 1
+
+# # Initialize the GroupConvLayerWithAttention
+# group_elements = [0, 90, 180, 270]  # Rotation angles in degrees
+# group_conv_layer = GroupConvLayerWithAttention(in_channels=3, out_channels=6,
+#                                                kernel_size=3, group=group_elements,
+#                                                learnable_rotations=True,
+#                                                # Add any additional parameters for attention here
+#                                                )
+
+# # Forward pass
+# output = group_conv_layer(dummy_input)
+
+# # Check tensor sizes
+# assert output.size(0) == dummy_input.size(0), "Mismatch in batch size"
+# assert output.size(1) != 0, "Output channels are zero"
+# assert output.size(2) == 64 and output.size(3) == 64, "Mismatch in spatial dimensions"
+
+# # Print the shape of the output tensor
+# print("Output shape:", output.shape)
+
+
+# from models.group_cnn_layer import GroupConvLayer, GroupConvLayerWithAttention
 
 # # Create a dummy input - a single image with 3 channels (RGB) and 64x64 pixels
 # dummy_input = torch.randn(1, 3, 64, 64)  # Batch size 1
@@ -52,7 +181,7 @@ class GroupConvLayer(nn.Module):
 # # Initialize the GroupConvLayer
 # group_elements = [0, 90, 180, 270]  # Example group elements (rotation angles in degrees)
 # group_conv_layer = GroupConvLayer(in_channels=3, out_channels=6, \
-#     kernel_size=3, group=group_elements)
+#     kernel_size=3, group=group_elements, learnable_rotations=True)
 
 # # Forward pass
 # output = group_conv_layer(dummy_input)
